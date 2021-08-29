0870db387fe8b70ef23afe4f0abb7313ac49679b
brandon6190 <brandonm6190@gmail.com>
2021-08-29 00:55:25 -0400

    Added the styling for the testimonial-slider and the slider functionality with jquery and slick libraries

--- assets/global.js
+++ assets/global.js
@@ -626,3 +626,30 @@ class VariantRadios extends VariantSelects {
 }
 
 customElements.define('variant-radios', VariantRadios);
+
+console.log('I am from global.js');
+
+$('.testimonial-slider').slick({
+  dots: true,
+  speed: 300,
+  infinite: false,
+  slidesToShow: 3,
+  slidesToSroll: 3,
+  arrows: false,
+  responsive: [
+      {
+          breakpoint: 989,
+          settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+          }
+      },
+      {
+          breakpoint: 750,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+          }
+      }
+  ]
+});
