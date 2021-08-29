bdbe388bb2a1a792116aa6fb21deaf31906472a9
brandon6190 <brandonm6190@gmail.com>
2021-08-29 14:06:36 -0400

    -Fixed some styling issues with testimonial cards -testimonial-slider is now dynamic

--- assets/global.js
+++ assets/global.js
@@ -629,27 +629,29 @@ customElements.define('variant-radios', VariantRadios);
 
 console.log('I am from global.js');
 
-$('.testimonial-slider').slick({
-  dots: true,
-  speed: 300,
-  infinite: false,
-  slidesToShow: 3,
-  slidesToSroll: 3,
-  arrows: false,
-  responsive: [
-      {
-          breakpoint: 989,
-          settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-          }
-      },
-      {
-          breakpoint: 750,
-          settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-          }
-      }
-  ]
+$(document).ready(function() {
+  $('.testimonial-slider').slick({
+    dots: true,
+    speed: 300,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToSroll: 3,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 989,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 750,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+  });
 });
